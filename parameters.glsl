bd15962ae3c167073f3b557d844db0b11ab77574
shourya-kr <shouryakumar0301@gmail.com>
2025-06-10 16:55:39 +0530

    Create parameters.glsl

---
+++ parameters.glsl
@@ -0,0 +1,6 @@
+// Light and material parameters
+const float kd = 1.0;      // diffuse strength
+const float ka = 1.0;      // ambient strength
+const float id = 0.6;      // diffuse light intensity
+const float ia = 0.2;      // ambient light intensity
+const vec3 lightDir = normalize(vec3(1,1,0)); // light direction
