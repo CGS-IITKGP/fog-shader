8403c983abea6a6b8d5e4ae6bc85d415cd2e12ab
shourya-kr <shouryakumar0301@gmail.com>
2025-06-29 01:25:37 +0530

    Update noise.glsl

--- noise.glsl
+++ noise.glsl
@@ -1,25 +1,28 @@
-// random and noise for fog modulation
-float rand2(vec2 st) {
-    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+//random generation function for 2d vector
+float random(vec2 p)
+{
+ return fract(sin(dot(p.xy,vec2(12.9898,78.233)))*43758.5453123);
 }
 
-float noise(vec2 st) {
-    vec2 i = floor(st);
-    vec2 f = fract(st);
-    float a = rand2(i);
-    float b = rand2(i + vec2(1.0, 0.0));
-    float c = rand2(i + vec2(0.0, 1.0));
-    float d = rand2(i + vec2(1.0, 1.0));
-    vec2 u = smoothstep(0.,1.,f);
-    return mix(a, b, u.x) + 
-           (c - a)*u.y*(1.0 - u.x) + 
-           (d - b)*u.x*u.y;
+//2D noise based on random function,smooth interpolated noise
+float noise(vec2 p) 
+{
+ vec2 i = floor(p);
+ vec2 f = fract(p);
+ float a = random(i);
+ float b = random(i + vec2(1.,.0));
+ float c = random(i + vec2(0.,1.));
+ float d = random(i + vec2(1.,1.));
+ vec2 u = smoothstep(0.,1.,f);
+ return mix(a,b,u.x) + (c-a)*u.y*(1.-u.x)+(d-b)*u.x*u.y;
 }
 
-float fbm(vec2 p) {
-    float f = 0.0;
-    f += 0.5 * noise(p); p *= 2.0;
-    f += 0.25 * noise(p); p *= 2.0;
-    f += 0.125 * noise(p);
-    return f;
+//fractal brownian motion for layered noise, used in fog movement
+float fbm(vec2 p)
+{
+ float f = 0.;
+ f += .5*noise(p);p *= 3.0;
+ f += .25*noise(p);p *= 3.0;
+ f += .125*noise(p);
+ return f;
 }
