3ee94ff265ff01467c06167b2c1d3b03ffda6521
shourya-kr <shouryakumar0301@gmail.com>
2025-06-29 01:23:59 +0530

    Update terrain.glsl

--- terrain.glsl
+++ terrain.glsl
@@ -1,11 +1,13 @@
-// terrain height
-float terrainHeight(float x, float z) {
-    return sin(x) * sin(z);
+//terrain height generation
+float terrainheight(float x,float z)
+{
+ return clamp(sin(x*2.)*sin(z)+sin(x)*sin(z*0.5),.0,2.);
 }
 
-// terrain normal
-vec3 calcNormal(vec3 p) {
-    float dx = sin(p.z) * cos(p.x);
-    float dz = sin(p.x) * cos(p.z);
-    return normalize(vec3(dx, 1.0, dz));
+//terrain normal(needed for lighting)
+vec3 normalcalc(vec3 p)
+{
+ float dx = sin(p.z)*cos(p.x*2.)*2.+sin(p.z*0.5)*cos(p.x);
+ float dz = sin(p.x)*cos(p.z)+sin(p.x)*cos(p.z*0.5)*0.5;
+ return normalize(vec3(dx,1.,dz));
 }
